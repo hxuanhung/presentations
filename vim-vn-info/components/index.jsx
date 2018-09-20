4f92320fd00ecdcfeeaf38802a11bcf692c8194a
Hung HOANG <hung@workwell.io>
2018-09-20 17:07:18 +0200

    vim vn info

---
+++ vim-vn-info/components/index.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Bold = styled.span`
+  font-weight: 600;
+  font-size: 54px;
+`;
+
+const PinkText = styled.div`
+  color: "#f0f";
+  font-size: "1em";
+`;
+
+const Box = styled.pre`
+  font-family: Menlo, monospace;
+  text-align: left;
+  white-space: pre-wrap;
+  font-size: 1em;
+  margin: 0px;
+  padding: 8px;
+  color: #f0f;
+  background-color: #333;
+`;
+
+export { Bold, PinkText, Box };
